b0f4fa6ed283b04fc7d5ab88d26d6ae0ba5ba0ce
minki0415 <minki5050@gmail.com>
2022-08-01 15:25:32 +0900

    add new files

--- step01_start/src/App.js
+++ step01_start/src/App.js
@@ -5,62 +5,91 @@ import Subject from './components/Subject';
 import Control from './components/Control';
 import TOC from './components/TOC';
 import CreateContent from './components/CreateContent';
+import UpdateContent from './components/UpdateContent';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.max_content_id = 3;
     this.state = {
-      mode:'read',
+      mode: 'read',
       selected_content_id: 2,
-      subject:{title:'WEB', sub:'World Wide Web!'},
-      welcome:{title:'WEB', desc:'Hello, React!!'},
-      contents:[
-        {id:1, title:'HTML', desc:'HTML is for information'},
-        {id:2, title:'CSS', desc:'CSS is for design'},
-        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'},
+      subject: { title: 'WEB', sub: 'World Wide Web!' },
+      welcome: { title: 'WEB', desc: 'Hello, React!!' },
+      contents: [
+        { id: 1, title: 'HTML', desc: 'HTML is for information' },
+        { id: 2, title: 'CSS', desc: 'CSS is for design' },
+        { id: 3, title: 'JavaScript', desc: 'JavaScript is for interactive' },
       ]
     }
   }
-  render() {
-    console.log('App render');
-    var _title, _desc = null, _article = null;
-    if(this.state.mode === 'welcome'){
+
+  getReadContent() {
+    var i = 0;
+    while (i < this.state.contents.length) {
+      var data = this.state.contents[i];
+      if (data.id === this.state.selected_content_id) {
+        return data;
+      }
+      i = i + 1;
+    }
+  }
+
+  getContent() {
+    var _title, _desc, _article = null;
+    if (this.state.mode === 'welcome') {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
       _article = <ReadContent title={_title} desc={_desc}></ReadContent>
-    } else if(this.state.mode === 'read') {
-      var i = 0;
-      while( i < this.state.contents.length) {
-        var data = this.state.contents[i];
-        if(data.id === this.state.selected_content_id) {
-          _title = data.title;
-          _desc = data.desc;
-          break;
-        }
-        i = i+1;
-      }
-      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
-    } else if(this.state.mode === 'create') {
-      _article = <CreateContent onSubmit= {function(_title, _desc) {
-        this.max_content_id = this.max_content_id+1;
-        var _contents = this.state.contents.concat(
-          {id:this.max_content_id, title:_title, desc:_desc}
-        );
+    } else if (this.state.mode === 'read') {
+      var _content = this.getReadContent();
+      _article = <ReadContent title={_content.title} desc={_content.desc}></ReadContent>
+    } else if (this.state.mode === 'create') {
+      _article = <CreateContent onSubmit={function (_title, _desc) {
+        this.max_content_id = this.max_content_id + 1;
+        var _contents = Array.from(this.state.contents);
+        _contents.push({id: this.max_content_id, title: _title, desc: _desc});
+        // var _contents = this.state.contents.concat(
+        //   { id: this.max_content_id, title: _title, desc: _desc }
+        // );
         this.setState({
-          contents:_contents
+          contents: _contents,
+          mode:'read',
+          selected_content_id:this.max_content_id
         });
         console.log(_title, _desc);
       }.bind(this)}></CreateContent>
+    } else if (this.state.mode === 'update') {
+      var _content = this.getReadContent();
+      _article = <UpdateContent data={_content} onSubmit={
+          function (_id, _title, _desc) {
+          var _contents = Array.from(this.state.contents);
+          var i = 0;
+          while(i < _contents.length) {
+            if(_contents[i].id === _id) {
+              _contents[i] = {id : _id, title: _title, desc:_desc};
+              break
+            }
+            i = i+1;
+          }
+          this.setState({
+            contents: _contents,
+            mode:'read'
+          });
+      }.bind(this)}></UpdateContent>
     }
-    console.log("render", this);
+    return _article;
+  }
+
+  render() {
+    console.log('App render');
     return (
       <div className='App'>
-        <Subject 
+        <Subject
           title={this.state.subject.title}
           sub={this.state.subject.sub}
           onChangePage={function () {
-            this.setState({mode:"welcome"});
+            this.setState({ mode: "welcome" });
           }.bind(this)}></Subject>
         {/*<header>
           <h1><a href='/' onClick={function(e) {
@@ -73,22 +102,22 @@ class App extends Component {
           bind(this) 를 통해 구현하면 함수 안에서 this가 우리의 App 컴포넌트를 가리키게 된다. 
           {this.state.subject.sub}
         </header>*/}
-          <TOC 
-            onChangePage={function (id) {
-              this.setState({
-                mode:"read",
-                selected_content_id:Number(id)
-              });
-            }.bind(this)}
-            data={this.state.contents}></TOC>
-          <Control onChangeMode={function(_mode) {
+        <TOC
+          onChangePage={function (id) {
             this.setState({
-              mode :_mode
+              mode: "read",
+              selected_content_id: Number(id)
             });
-          }.bind(this)}></Control>
-          {_article}
-        </div>
-      )
+          }.bind(this)}
+          data={this.state.contents}></TOC>
+        <Control onChangeMode={function (_mode) {
+          this.setState({
+            mode: _mode
+          });
+        }.bind(this)}></Control>
+        {this.getContent()}
+      </div>
+    )
   }
 }
 
