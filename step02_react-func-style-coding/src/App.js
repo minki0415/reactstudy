2a4af0c239c110774ba824856e8216751d4717c2
minki0415 <minki5050@gmail.com>
2022-08-02 18:52:49 +0900

    add new files

--- step02_react-func-style-coding/src/App.js
+++ step02_react-func-style-coding/src/App.js
@@ -15,14 +15,27 @@ function FuncComp(props) {
   var numberState = useState(props.initNumber);
   var number = numberState[0];
   var setNumber = numberState[1];
-  console.log('numberState', numberState);
+
+  var dateState = useState((new Date()).toString());
+  var _date = dateState[0];
+  var setDate = dateState[1];
+
+  // useState는 2개의 요소를 갖는 배열, 첫번째가 우리가 원하는 state값, 
+  // console.log('numberState', numberState);
+
   return(
     <div className='container'>
       <h2>func style component</h2>
       <p>Number : {number}</p>
+      <p>Date : {_date}</p>
       <input type="button" value="random" onClick= {
         function() {
           setNumber(Math.random());
+        }
+      }></input>
+      <input type="button" value="date" onClick={
+        function() {
+          setDate((new Date().toString()));
         }
       }></input>
     </div>
@@ -31,16 +44,23 @@ function FuncComp(props) {
 
 class ClassComp extends React.Component{
   state = {
-    number:this.props.initNumber
+    number:this.props.initNumber,
+    date:(new Date()).toString()
   }
   render() {
     return (
       <div className='container'>
         <h2>class style component</h2>
         <p>Number : {this.state.number}</p>
+        <p>Date : {this.state.date}</p>
         <input type="button" value="random" onClick={
           function() {
             this.setState({number:Math.random()})
+          }.bind(this)
+        }></input>
+        <input type="button" value="date" onClick={
+          function() {
+            this.setState({date:(new Date()).toString()});
           }.bind(this)
         }></input>
       </div>
