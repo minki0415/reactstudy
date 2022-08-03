d7a6e521ce9e717854cd0bba1c7db27742af5113
minki0415 <minki5050@gmail.com>
2022-08-03 17:51:46 +0900

    add new AJAX study files

---
+++ step04_react-ajax/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>WEB</h1>
+      <nav>
+        <ul>
+          <li><a href='1'>HTML</a></li>
+          <li><a href='2'>CSS</a></li>
+          <li><a href='3'>JavaScript</a></li>
+        </ul>
+      </nav>
+      <article>
+        <h2>Welcome</h2>
+        Hello, React &amp; AJAX
+      </article>
+    </div>
+  );
+}
+
+export default App;
